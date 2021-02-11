3080ecd650aedc60eb791343e11fce4e7b561931
Rahamathali <rahamathalisk@gmail.com>
2021-02-11 12:50:48 +0300

    commited

--- src/main/java/com/employee/employee/EmployeeApplication.java
+++ src/main/java/com/employee/employee/EmployeeApplication.java
@@ -9,18 +9,5 @@ public class EmployeeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
-
-	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("http://domain1.com");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
-	}
+  
 }
